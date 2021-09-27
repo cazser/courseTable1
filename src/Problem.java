f90448cbdc60a158ffd8fcb9872d7a5c5a1f7c7c
cazser <2264712072@qq.com>
2021-09-27 13:14:29 +0800

    把所有课程加上也没问题

--- src/Problem.java
+++ src/Problem.java
@@ -414,13 +414,6 @@ public class Problem {
                 0,0,0,0});
 
 
-
-
-
-
-
-
-
         timetable.addModule(1,"高级语言程序设计1", "高级语言程序设计1", new int[]{1});
         timetable.addModule(2,"高级语言程序设计(实)", "高级语言程序设计(实)", new int[]{1});
         timetable.addModule(3,"高级语言程序设计2", "高级语言程序设计2", new int[]{1});
@@ -468,7 +461,224 @@ public class Problem {
         timetable.addModule(42,"高级语言程序设计1", "高级语言程序设计1", new int[]{15});
         timetable.addModule(43,"高级语言程序设计(实)", "高级语言程序设计(实)", new int[]{15});
         timetable.addModule(44,"高级语言程序设计2", "高级语言程序设计2", new int[]{15});
-        // Set up student groups and the modules they take.
+
+        timetable.addModule(45, "数据结构", "数据结构", new int[]{16});
+        timetable.addModule(46,"人工智能导论", "人工智能导论", new int[]{17});
+        timetable.addModule(47, "计算机辅助教学", "计算机辅助教学", new int[]{18});
+        timetable.addModule(48, "数据结构", "数据结构", new int[]{16});
+        timetable.addModule(49, "Python程序设计", "Python程序设计", new int[]{19});
+        timetable.addModule(50, "web程序设计与框架应用(单)Python程序设计(双)",  
+                                "web程序设计与框架应用(单)Python程序设计(双)", new int[]{19, 20});
+        timetable.addModule(51, "人工智能导论1-8web程序设计与框架应用9-16", 
+                                "人工智能导论1-8web程序设计与框架应用9-16",  new int[]{20, 17});
+
+        timetable.addModule(52, "数据结构", "数据结构", new int[]{16});
+        timetable.addModule(53,"人工智能导论", "人工智能导论", new int[]{17});
+        timetable.addModule(54,"人工智能导论(实）", "人工智能导论（实）", new int[]{17});
+        timetable.addModule(55, "计算机辅助教学", "计算机辅助教学", new int[]{18});
+        timetable.addModule(56, "数据结构", "数据结构", new int[]{16});
+        timetable.addModule(57, "Python程序设计1", "Python程序设计1", new int[]{19});
+        timetable.addModule(58, "Python程序设计2", "Python程序设计2", new int[]{19});
+        timetable.addModule(59, "web程序设计与框架应用", 
+                                "web程序设计与框架应用",  new int[]{20});
+        timetable.addModule(60, "web程序设计与框架应用(实)", 
+                                "web程序设计与框架应用(实)",  new int[]{20});
+
+        timetable.addModule(61, "web程序设计", "web程序设计", new int[]{1});
+        timetable.addModule(62, "数据结构", "数据结构", new int[]{21});
+        timetable.addModule(63,"人工智能导论", "人工智能导论", new int[]{22});
+        timetable.addModule(64, "单片机原理与应用", "单片机原理与应用", new int[]{23});
+        timetable.addModule(65, "设计模式", "设计模式", new int[]{24});
+        timetable.addModule(66, "数字逻辑", "数字逻辑", new int[]{25});
+        timetable.addModule(67,"人工智能导论(实)", "人工智能导论(实)", new int[]{22});
+        
+        timetable.addModule(68, "web程序设计", "web程序设计", new int[]{16});
+        timetable.addModule(69, "数据结构", "数据结构", new int[]{21});
+        timetable.addModule(70,"人工智能导论", "人工智能导论", new int[]{22});
+        timetable.addModule(71, "单片机原理与应用", "单片机原理与应用", new int[]{23});
+        timetable.addModule(72, "设计模式", "设计模式", new int[]{24});
+        timetable.addModule(73, "数字逻辑", "数字逻辑", new int[]{25});
+        timetable.addModule(74,"人工智能导论(实)", "人工智能导论(实)", new int[]{22});
+
+        timetable.addModule(75, "Python程序设计", "Python程序设计", new int[]{19});
+        timetable.addModule(76, "软件需求分析", "软件需求分析", new int[]{26});
+        timetable.addModule(77, "web程序设计", "web程序设计", new int[]{27});
+        timetable.addModule(78, "数据结构", "数据结构", new int[]{28});
+        timetable.addModule(79, "web前端开发技术", "web前端开发技术", new int[]{29});
+        timetable.addModule(80, "web程序设计(实)", "web程序设计(实)", new int[]{27});
+        timetable.addModule(81, "计算机系统基础", "计算机系统基础", new int[]{30});
+        timetable.addModule(82, "数据结构2", "数据结构2", new int[]{28});
+        timetable.addModule(83, "计算机系统基础2", "计算机系统基础2", new int[]{30});
+
+        timetable.addModule(84, "Python程序设计", "Python程序设计", new int[]{19});
+        timetable.addModule(85, "软件需求分析", "软件需求分析", new int[]{26});
+        timetable.addModule(86, "web程序设计", "web程序设计", new int[]{27});
+        timetable.addModule(87, "数据结构", "数据结构", new int[]{28});
+        timetable.addModule(88, "web前端开发技术", "web前端开发技术", new int[]{29});
+        timetable.addModule(89, "计算机系统基础", "计算机系统基础", new int[]{30});
+        timetable.addModule(90, "数据结构2", "数据结构2", new int[]{28});
+        timetable.addModule(91, "计算机系统基础2", "计算机系统基础2", new int[]{30});
+
+        timetable.addModule(92, "计算机图形学", "计算机图形学", new int[]{31});
+        timetable.addModule(93, "影视特效制作|c方向5-7,游戏原画设计|c方向1",  
+                                "影视特效制作|c方向5-7,游戏原画设计|c方向1",  new int[]{32, 33});
+        timetable.addModule(94, "影视特效制作|c方向5-7,游戏原画设计|c方向2",  
+                                "影视特效制作|c方向5-7,游戏原画设计|c方向2",  new int[]{32, 33});
+        timetable.addModule(95, "影视特效制作|c方向5-7,游戏原画设计|c方向3",  
+                                "影视特效制作|c方向5-7,游戏原画设计|c方向3",  new int[]{32, 33});
+        timetable.addModule(96, "影视特效制作|c方向5-7,游戏原画设计|c方向4",  
+                                "影视特效制作|c方向5-7,游戏原画设计|c方向4",  new int[]{32, 33});
+        timetable.addModule(97, "操作系统API事件",  "操作系统API事件", new int[]{34});
+        timetable.addModule(98, "数据结构", "数据结构", new int[]{35});
+        timetable.addModule(99, "数据结构(实)", "数据结构(实)", new int[]{35});
+        timetable.addModule(100, "影视特效制作|c方向5-7,三维动画制作技术|c方向1",  
+                                "影视特效制作|c方向5-7,三维动画制作技术|c方向1",  new int[]{32, 33});
+        timetable.addModule(101, "影视特效制作|c方向5-7,三维动画制作技术|c方向2",
+                                "影视特效制作|c方向5-7,三维动画制作技术|c方向2" ,  new int[]{32, 33});
+        timetable.addModule(102, "三维动画制作技术", "三维动画制作技术", new int[]{36});
+        
+        timetable.addModule(103, "数据库系统原理", "数据库系统原理", new int[]{37});
+        timetable.addModule(104, "数据结构", "数据结构", new int[]{38});
+        timetable.addModule(105, "数字电子技术", "数字电子技术", new int[]{39});
+        timetable.addModule(106, "物联网专业英语", "物联网专业英语", new int[]{40});
+        timetable.addModule(107, "Python语言", "Python语言", new int[]{41});
+        timetable.addModule(108, "Python语言(实)", "Python语言(实)", new int[]{41});
+        timetable.addModule(109, "数据库应用与开发1班", "数据库应用与开发1班", new int[]{37});
+        timetable.addModule(110, "数据库应用与开发2班", "数据库应用与开发2班", new int[]{37});
+
+        timetable.addModule(111, "Python程序设计", "Python程序设计", new int[]{12});
+        timetable.addModule(112, "数据结构", "数据结构", new int[]{15});
+        timetable.addModule(113, "数字逻辑", "数字逻辑", new int[]{42});
+        timetable.addModule(114, "大数据原理与应用",  "大数据原理与应用", new int[]{42});
+        timetable.addModule(115, "数据库系统原理1-8&&web程序设计9-16",  "数据库系统原理1-8&&web程序设计9-16",new int[]{12,43});
+        timetable.addModule(116, "数据库系统原理",  "数据库系统原理",new int[]{43});
+        timetable.addModule(117, "数据结构2", "数据结构2", new int[]{15});
+        timetable.addModule(118, "数字逻辑1-8web程序设计9-16",   "数字逻辑1-8web程序设计9-16",new int[]{12, 42});
+        timetable.addModule(119, "大数据原理与应用(实)",  "大数据原理与应用(实)", new int[]{42});
+        
+        timetable.addModule(120, "计算机网络",  "计算机网络", new int[]{44});
+        timetable.addModule(121, "编译原理",  "编译原理", new int[]{24});
+        timetable.addModule(122, "操作系统",  "操作系统", new int[]{45});
+        timetable.addModule(123, "数据库设计与应用",  "数据库设计与应用", new int[]{46});
+        timetable.addModule(124, "信息技术课程标准与教材研究",   "信息技术课程标准与教材研究", new int[]{47});
+        timetable.addModule(125, "智能教育工程项目设计1-8机器人教育9-16",   
+                                 "智能教育工程项目设计1-8机器人教育9-16", new int[]{17, 5});
+        timetable.addModule(126, "信息技术课程标准与教材研究1-8&&计算机网络",   
+                                 "信息技术课程标准与教材研究1-8&&计算机网络",  new int[]{47, 44});
+        timetable.addModule(127, "智能教育工程项目设计1-8编译原理9-16",   
+                                 "智能教育工程项目设计1-8编译原理9-16", new int[]{17, 24});
+        timetable.addModule(128, "操作系统",  "操作系统", new int[]{45});
+        timetable.addModule(129, "数据库设计与应用",  "数据库设计与应用", new int[]{46});
+        timetable.addModule(130, "机器人教育", "机器人教育", new int[]{5});
+
+        timetable.addModule(131, "计算机网络",  "计算机网络", new int[]{44});
+        timetable.addModule(132, "编译原理",  "编译原理", new int[]{24});
+        timetable.addModule(133, "信息技术课程标准与教材研究",   "信息技术课程标准与教材研究", new int[]{47});
+        timetable.addModule(134, "操作系统",  "操作系统", new int[]{48});
+        timetable.addModule(135, "智能教育工程项目设计",   "智能教育工程项目设计", new int[]{17});
+        timetable.addModule(136, "数据库设计与应用",  "数据库设计与应用", new int[]{46});
+        timetable.addModule(137, "智能教育工程项目设计1-8编译原理9-16",   
+                                 "智能教育工程项目设计1-8编译原理9-16", new int[]{17, 24});
+        timetable.addModule(138, "信息技术课程标准与教材研究1-8&&计算机网络",   
+                                 "信息技术课程标准与教材研究1-8&&计算机网络",  new int[]{47, 44});
+        timetable.addModule(139, "数据库设计与应用",  "数据库设计与应用", new int[]{46});
+        timetable.addModule(140, "机器人教育", "机器人教育", new int[]{5});
+
+        timetable.addModule(141, "编译原理",  "编译原理", new int[]{22});
+        timetable.addModule(142, "操作系统",  "操作系统", new int[]{45});
+        timetable.addModule(143, "C++程序设计",  "C++程序设计", new int[]{18});
+        timetable.addModule(144, "ARM体系结构",  "ARM体系结构", new int[]{ 7});
+        timetable.addModule(145, "数据库设计与应用",  "数据库设计与应用", new int[]{29});
+        timetable.addModule(146, "操作系统",  "操作系统", new int[]{45});
+        timetable.addModule(147, "计算机网络1-8编译原理9-16",   "计算机网络1-8编译原理9-16",  new int[]{44, 22});
+        timetable.addModule(148, "C++程序设计",  "C++程序设计", new int[]{18});
+        timetable.addModule(149, "数据库设计与应用",  "数据库设计与应用", new int[]{29});
+        timetable.addModule(150, "计算机网络",  "计算机网络", new int[]{42});
+        timetable.addModule(151, "单片机原理与应用",  "单片机原理与应用", new int[]{23});
+        
+        timetable.addModule(152, "SSH开发技术",   "SSH开发技术", new int[]{ 6});
+        timetable.addModule(153, "编译原理",  "编译原理", new int[]{22});
+        timetable.addModule(154, "数据库设计与应用",  "数据库设计与应用", new int[]{7});
+        timetable.addModule(155, "UML统一建模",  "UML统一建模", new int[]{ 46 });
+        timetable.addModule(156, "操作系统",  "操作系统", new int[]{45});
+        timetable.addModule(157, "SSH开发技术",   "SSH开发技术", new int[]{ 6});
+        timetable.addModule(158, "计算机网络1-8编译原理9-16",   "计算机网络1-8编译原理9-16",  new int[]{44, 22});
+        timetable.addModule(159, "UML统一建模",  "UML统一建模", new int[]{ 46 });
+        timetable.addModule(160, "操作系统1-8&&数据库设计与应用",   "操作系统1-8&&数据库设计与应用", new int[]{45, 7});
+        timetable.addModule(161, "单片机原理与应用",  "单片机原理与应用", new int[]{23});
+        timetable.addModule(162, "计算机网络",   "计算机网络",  new int[]{44 });
+        
+        timetable.addModule(163, "操作系统",  "操作系统", new int[]{49});
+        timetable.addModule(164, "UML统一建模",  "UML统一建模", new int[]{ 50 });
+        timetable.addModule(165, "软件测试与质量保证",   "软件测试与质量保证", new int[]{ 51 });
+        timetable.addModule(166, "移动终端应用开发基础",   "移动终端应用开发基础",  new int[]{ 9  });
+        timetable.addModule(167, "软件需求分析",  "软件需求分析", new int[]{ 26 });
+        timetable.addModule(168, "软件测试与质量保证",   "软件测试与质量保证", new int[]{ 51 });
+        timetable.addModule(169, "操作系统",  "操作系统", new int[]{49});
+        timetable.addModule(170, "个人级软件开发",  "个人级软件开发", new int[]{ 8});
+        timetable.addModule(171, "计算机组成原理1-8个人级软件开发9-16",   "计算机组成原理1-8个人级软件开发9-16",  new int[]{30, 8});
+        timetable.addModule(172, "软件框架开发技术1-8个人级软件开发9-16",  "个人级软件开发", new int[]{20, 52});
+        timetable.addModule(173, "软件框架开发技术1-8个人级软件开发9-16",  "个人级软件开发", new int[]{20, 52});
+       
+        timetable.addModule(174, "UML统一建模",  "UML统一建模", new int[]{ 50 });
+        timetable.addModule(175, "操作系统",  "操作系统", new int[]{49});
+        timetable.addModule(176, "计算机组成原理",   "计算机组成原理",  new int[]{ 30 });
+        timetable.addModule(177, "移动终端应用开发基础",   "移动终端应用开发基础",  new int[]{ 9  });
+        timetable.addModule(178, "软件测试与质量保证",   "软件测试与质量保证", new int[]{ 51 });
+        timetable.addModule(179, "软件测试与质量保证",   "软件测试与质量保证", new int[]{ 51 });
+        timetable.addModule(180, "软件需求分析",  "软件需求分析", new int[]{ 26 });
+        timetable.addModule(181, "软件框架开发技术1-8个人级软件开发9-16",  "个人级软件开发", new int[]{20, 29});
+        timetable.addModule(182, "软件框架开发技术1-8个人级软件开发9-16",  "个人级软件开发", new int[]{20, 29});
+        timetable.addModule(183, "个人级软件开发",  "个人级软件开发", new int[]{ 27 });
+        timetable.addModule(184, "计算机组成原理1-8个人级软件开发9-16",    "计算机组成原理1-8个人级软件开发9-16",  new int[]{ 30, 27 });
+         
+        timetable.addModule(185, "计算机图形学",  "计算机图形学", new int[]{ 31 });
+        timetable.addModule(186, "Unity3D",  "Unity3D", new int[]{ 34 });
+        timetable.addModule(187, "游戏引擎原理与实现",  "游戏引擎原理与实现", new int[]{ 28 });
+        timetable.addModule(188, "ANDROID平台程序设计",  "ANDROID平台程序设计", new int[]{ 12 });
+        timetable.addModule(189, "游戏引擎原理与实现",  "游戏引擎原理与实现", new int[]{ 28 });
+        timetable.addModule(190, "ANDROID平台程序设计1-8游戏职业与素质拓展9-16",   
+                                 "ANDROID平台程序设计1-8游戏职业与素质拓展9-16", new int[]{ 12 , 31});
+        timetable.addModule(191, "计算机网络",  "计算机网络", new int[]{ 34 });
+        timetable.addModule(192, "计算机网络(单)计算机图形学(双)",   "计算机网络(单)计算机图形学(双)", new int[]{ 34 , 31});
+        timetable.addModule(193, "游戏引擎原理与实现",  "游戏引擎原理与实现", new int[]{ 28 });
+        timetable.addModule(194, "游戏场景设计", "游戏场景设计", new int[]{35});
+        timetable.addModule(195, "游戏英语", "游戏英语", new int[]{ 53 });
+
+        timetable.addModule(196, "数据处理与智能决策", "数据处理与智能决策", new int[]{ 54 });
+        timetable.addModule(197, "物联网通信技术", "物联网通信技术", new int[]{ 55 });
+        timetable.addModule(198, "物联网通信课程设计", "物联网通信课程设计", new int[]{ 55 });
+        timetable.addModule(199, "物联网通信课程设计", "物联网通信课程设计", new int[]{ 55 });
+        timetable.addModule(200, "数据处理与智能决策", "数据处理与智能决策", new int[]{ 54 });
+        timetable.addModule(201, "物联网通信技术", "物联网通信技术", new int[]{ 55 });
+        timetable.addModule(202, "物联网通信课程设计", "物联网通信课程设计", new int[]{ 55 });
+        timetable.addModule(203, "物联网通信课程设计", "物联网通信课程设计", new int[]{ 55 });
+        timetable.addModule(204, "计算机网络",  "计算机网络", new int[]{ 14 });
+        timetable.addModule(205, "Linux高级服务应用",  "Linux高级服务应用", new int[]{  40});
+        timetable.addModule(206, "计算机网络",  "计算机网络", new int[]{ 14 });
+        timetable.addModule(207, "云计算技术1-8Linux程序设计9-16",   
+                                 "云计算技术1-8Linux程序设计9-16",  new int[]{ 41 });
+        timetable.addModule(208, "云计算技术1-8Linux程序设计9-16",   
+                                 "云计算技术1-8Linux程序设计9-16",  new int[]{ 41 });
+        timetable.addModule(209, "密码学基础",  "密码学基础", new int[]{  40});
+        
+        timetable.addModule(210, "数据挖掘",  "数据挖掘", new int[]{  56});
+        timetable.addModule(211, "软件工程",  "软件工程", new int[]{  56});
+        timetable.addModule(212, "数据采集",  "数据采集", new int[]{  57});
+        timetable.addModule(213, "数据采集(实)",  "数据采集(实)", new int[]{  57});
+        timetable.addModule(214, "数据挖掘",  "数据挖掘", new int[]{  56});
+        timetable.addModule(215, "软件工程",  "软件工程", new int[]{  56});
+        timetable.addModule(216, "计算机网络",  "计算机网络", new int[]{ 50 });
+        timetable.addModule(217, "Spark大数据快速运算",  "Spark大数据快速运算", new int[]{ 58 });
+        timetable.addModule(218, "Spark大数据快速运算",  "Spark大数据快速运算", new int[]{ 58 });
+        timetable.addModule(219, "计算机网络",  "计算机网络", new int[]{ 50 });
+        timetable.addModule(220, "数据清洗",  "数据清洗", new int[]{  57});
+        timetable.addModule(221, "数据清洗(实)",  "数据清洗(实)", new int[]{  57});
+        
+
+
+
+       // Set up student groups and the modules they take.
         timetable.addGroup(1, 100, new int[] { 1,2, 3,  9, 10},
                                                     new int[]{2,3,6, 10,11,12,15, 16,18});
         this.Grouprefer.put(1, "2021计科1班公费");
